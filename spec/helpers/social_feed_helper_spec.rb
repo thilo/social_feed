4cf859e25db2223564cbc6e487b6b48659307279
Thilo <thilo@Silverchest.local>
2008-06-05 17:05:57 +0200

    fixed that the wrong tempalte name is used if the word event appears more than ones in an event type

---
+++ spec/helpers/social_feed_helper_spec.rb
@@ -0,0 +1,17 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+require PLUGIN_ROOT + '/lib/social_feed/social_feed_helper'
+include SocialFeed::SocialFeedHelper
+class TestEvent; end
+class TestEventCreatedEvent; end
+
+describe SocialFeed::SocialFeedHelper do
+  
+  it "should derive the hint partial form the feed class" do
+    feed_event_partial_name(TestEvent.new).should == 'test_hint'
+  end
+  
+  it "should only replace event with hint on the end" do
+    feed_event_partial_name(TestEventCreatedEvent.new).should == 'test_event_created_hint'
+  end
+  
+end
